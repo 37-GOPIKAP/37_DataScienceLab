3b57d6d94e2c0b6b6770e729b1b699e88456e389
Gopika P <62229869+37-GOPIKAP@users.noreply.github.com>
2021-12-03 11:16:00 +0530

    Delete Fileprgm2.py

--- Fileprgm2.py
+++
@@ -1,8 +0,0 @@
-name1=input("Enter the file to be read from:")
-name2=input("Enter the file to be appended to:")
-file=open(name1,"r")
-data2=file.read()
-file.close()
-fout=open(name2,"a")
-fout.write(data2)
-fout.close()
